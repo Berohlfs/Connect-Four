5195618d031a426ec10a5d9ae70be62976f53c09
Bernardo Cruz Rohlfs <90404673+Berohlfs@users.noreply.github.com>
2023-01-05 00:29:13 -0300

    Delete Condicionais de WSituations

--- code/script.js
+++ code/script.js
@@ -15,60 +15,15 @@ function colorir(x) {
             counter++
         }
     }
-    // ====================
-    if (divs[0].style.backgroundColor == 'red' && divs[1].style.backgroundColor == 'red' && divs[2].style.backgroundColor == 'red') {
-        document.querySelector('h1').innerText = `Vencedor: Vermelho!`
-    }
-    if (divs[0].style.backgroundColor == 'green' && divs[1].style.backgroundColor == 'green' && divs[2].style.backgroundColor == 'green') {
-        document.querySelector('h1').innerText = `Vencedor: Verde!`
-    }
-    // ====================
-    if (divs[3].style.backgroundColor == 'red' && divs[4].style.backgroundColor == 'red' && divs[5].style.backgroundColor == 'red') {
-        document.querySelector('h1').innerText = `Vencedor: Vermelho!`
-    }
-    if (divs[3].style.backgroundColor == 'green' && divs[4].style.backgroundColor == 'green' && divs[5].style.backgroundColor == 'green') {
-        document.querySelector('h1').innerText = `Vencedor: Verde!`
-    }
-    // ====================
-    if (divs[6].style.backgroundColor == 'red' && divs[7].style.backgroundColor == 'red' && divs[8].style.backgroundColor == 'red') {
-        document.querySelector('h1').innerText = `Vencedor: Vermelho!`
-    }
-    if (divs[6].style.backgroundColor == 'green' && divs[7].style.backgroundColor == 'green' && divs[8].style.backgroundColor == 'green') {
-        document.querySelector('h1').innerText = `Vencedor: Verde!`
-    }
-    // ====================
-    if (divs[0].style.backgroundColor == 'red' && divs[3].style.backgroundColor == 'red' && divs[6].style.backgroundColor == 'red') {
-        document.querySelector('h1').innerText = `Vencedor: Vermelho!`
-    }
-    if (divs[0].style.backgroundColor == 'green' && divs[3].style.backgroundColor == 'green' && divs[6].style.backgroundColor == 'green') {
-        document.querySelector('h1').innerText = `Vencedor: Verde!`
-    }
-    // ====================
-    if (divs[1].style.backgroundColor == 'red' && divs[4].style.backgroundColor == 'red' && divs[7].style.backgroundColor == 'red') {
-        document.querySelector('h1').innerText = `Vencedor: Vermelho!`
-    }
-    if (divs[1].style.backgroundColor == 'green' && divs[4].style.backgroundColor == 'green' && divs[7].style.backgroundColor == 'green') {
-        document.querySelector('h1').innerText = `Vencedor: Verde!`
-    }
-    // ====================
-    if (divs[2].style.backgroundColor == 'red' && divs[5].style.backgroundColor == 'red' && divs[8].style.backgroundColor == 'red') {
-        document.querySelector('h1').innerText = `Vencedor: Vermelho!`
-    }
-    if (divs[2].style.backgroundColor == 'green' && divs[5].style.backgroundColor == 'green' && divs[8].style.backgroundColor == 'green') {
-        document.querySelector('h1').innerText = `Vencedor: Verde!`
-    }
-    // ====================
-    if (divs[0].style.backgroundColor == 'red' && divs[4].style.backgroundColor == 'red' && divs[8].style.backgroundColor == 'red') {
-        document.querySelector('h1').innerText = `Vencedor: Vermelho!`
-    }
-    if (divs[0].style.backgroundColor == 'green' && divs[4].style.backgroundColor == 'green' && divs[8].style.backgroundColor == 'green') {
-        document.querySelector('h1').innerText = `Vencedor: Verde!`
-    }
-    // ====================
-    if (divs[2].style.backgroundColor == 'red' && divs[4].style.backgroundColor == 'red' && divs[6].style.backgroundColor == 'red') {
-        document.querySelector('h1').innerText = `Vencedor: Vermelho!`
-    }
-    if (divs[2].style.backgroundColor == 'green' && divs[4].style.backgroundColor == 'green' && divs[6].style.backgroundColor == 'green') {
-        document.querySelector('h1').innerText = `Vencedor: Verde!`
-    }
 }
+
+const win = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]

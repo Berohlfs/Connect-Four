d04e9010351d1ee249fbcea9526f628a000d6899
Bernardo Cruz Rohlfs <90404673+Berohlfs@users.noreply.github.com>
2023-01-05 11:24:10 -0300

    Update script - versão 2.0
    Deu certo!

--- code/script.js
+++ code/script.js
@@ -2,12 +2,7 @@ var counter_color = true;
 const divs = document.getElementsByClassName('inner');
 const red = 'red'
 const green = 'green'
-
-for (i = 0; i < 9; i++) {
-    divs[i].style.backgroundColor = 'darkblue';
-}
-
-const win = [
+const win_situations = [
     [0,1,2],
     [3,4,5],
     [6,7,8],
@@ -15,28 +10,11 @@ const win = [
     [1,4,7],
     [2,5,8],
     [0,4,8],
-    [2,4,6],
+    [2,4,6]
 ]
 
-function winsituation(){
-    var counter_win;
-    var turn;
-    for(i=0;i<2;i++){
-        i == 0 ? turn = green : turn = red;
-        for(j=0;j<9;j++){
-            if(counter_win == 3){
-                break;
-            }
-            counter_win = 0;
-            for(k=0;k<3;k++){
-                divs[win[j][k]].style.backgroundColor == turn && counter_win++ 
-                if(counter_win == 3){
-                    document.querySelector('h1').innerText = `Vencedor : ${turn}!`
-                    break;
-                }
-            }
-        }
-    }
+for (i = 0; i < 9; i++) {
+    divs[i].style.backgroundColor = 'darkblue';
 }
 
 function colorir(x) {
@@ -50,7 +28,29 @@ function colorir(x) {
             counter_color=true;
         }
     }
-    winsituation();
+    isWin();
+}
+
+function isWin(){
+    var counter_win;
+    var turn;
+    for(i=0;i<2;i++){
+        i == 0 ? turn = green : turn = red;
+        console.log(turn)
+        for(j=0;j<8;j++){
+            if(counter_win == 3){
+                break;
+            }
+            counter_win = 0;
+            for(k=0;k<3;k++){
+                console.log(win_situations[j][k])
+                divs[win_situations[j][k]].style.backgroundColor == turn && counter_win++ 
+                if(counter_win == 3){
+                    document.querySelector('h1').innerText = `Vencedor : ${turn}!`
+                }
+            }
+        }
+    }
 }
 
 function reload(){

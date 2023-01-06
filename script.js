7d0ea3572f274f8b7a2759e8be174c0bb8a86399
Bernardo Cruz Rohlfs <90404673+Berohlfs@users.noreply.github.com>
2023-01-06 12:51:16 -0300

    Connect Four Version

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
 var toogle_color = true;
 var pode_colorir = true;
+let counter_draw;
 const divs = document.getElementsByClassName('inner');
 const red = 'red'
 const green = 'green'
 const win_situations = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0,1,2,3],
+    [1,2,3,4],
+    [5,6,7,8],
+    [6,7,8,9],
+    [10,11,12,13],
+    [11,12,13,14],
+    [15,16,17,18],
+    [16,17,18,19],
+    [20,21,22,23],
+    [21,22,23,24],
+    [0,5,10,15],
+    [5,10,15,20],
+    [1,6,11,16],
+    [6,11,16,21],
+    [2,7,12,17],
+    [7,12,17,22],
+    [3,8,13,18],
+    [8,13,18,23],
+    [4,9,14,19],
+    [9,14,19,24],
+    [5,11,17,23],
+    [0,6,12,18],
+    [6,12,18,24],
+    [1,7,13,19],
+    [3,7,11,15],
+    [4,8,12,16],
+    [8,12,16,20],
+    [9,13,17,21]
 ]
 
 function fill(){
-    for (i = 0; i < 9; i++) {
+    for (i = 0; i < 25; i++) {
         divs[i].style.backgroundColor = 'darkblue';
     }
     document.querySelector('h1').innerText = `Winner :`
     pode_colorir = true;
+    counter_draw = 0;
 }
 
 function colorir(x) {
@@ -34,37 +56,32 @@ function colorir(x) {
             toogle_color=true;
             isWin();
         }
+        counter_draw++
+    }
+    if(counter_draw == 25){
+        document.querySelector('h1').innerText = `Draw!`
     }
 }
 
 function isWin(){
     let counter_win;
     let turn;
-    let counter_draw = 0;
     for(i=0;i<2;i++){
         i == 0 ? turn = green : turn = red;
         console.log(turn)
-        for(j=0;j<8;j++){
-            if(counter_win == 3){
+        for(j=0;j<28;j++){
+            if(counter_win == 4){
                 break;
             }
             counter_win = 0;
-            for(k=0;k<3;k++){
+            for(k=0;k<4;k++){
                 console.log(win_situations[j][k])
                 divs[win_situations[j][k]].style.backgroundColor == turn && counter_win++ 
-                if(counter_win == 3){
+                if(counter_win == 4){
                     document.querySelector('h1').innerText = `Winner : ${turn}!`
                     pode_colorir = false;
                 }
             }
-        }
-    }
-    for(i=0;i<9;i++){
-        if(divs[i].style.backgroundColor != 'darkblue' && counter_win<3){
-            counter_draw++
-        }
-        if(counter_draw == 9){
-            document.querySelector('h1').innerText = `Draw!`
         }
     }
 }
